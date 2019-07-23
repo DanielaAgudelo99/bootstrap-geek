821517d11f3c1f94aa3b2fd22e5b303077266897
Sala 310 <sala310@poliacademico.edu.loc>
2019-07-23 12:40:14 -0500

    geek-conf

---
+++ geek-conf/src/components/Slider.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+
+<main id="main">
+        <div className="carousel" className="carousel slide carousel-fade" data-ride="carousel" data-pause="false">
+            <div className="carousel-inner">
+                <div className="carousel-item active">
+                    <img src="assets/img/medellin1.png" className="d-block w-100" alt="Medellin 1"/>
+                </div>
+                <div className="carousel-item">
+                    <img src="assets/img/medellin2.png" className="d-block w-100" alt="Medellin 2"/>
+                </div>
+                <div className="carousel-item">
+                    <img src="assets/img/medellin3.png" className="d-block w-100" alt="Medellin 3"/>
+                </div>
+                <div className="overlay">
+                    <div className="container">
+                        <div className="row align-items-center">
+                            <div className="col-md-6 offset-md-6 text-center text-md-right">
+                                <h1>Geek Conf Medellín</h1>
+                                <p class="d-none d-md-block">Geek conf realiza su primer Demo Day con la cohorte de 
+                                    Línea 1: FrontEnd, donde los Geeks entrenados evidencian
+                                    por medio de un proyecto todas las habilidades y conocimientos
+                                    técnicos adquiridos durante el curso.
+                                </p>
+                                <a href="#" className="btn btn-outline-light">Quiero ser orador</a>
+                                <button type="button" className="btn btn-geek" data-toggle="modal" data-target="#modalCompra">Comprar Tickets</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
---
+++ geek-conf/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './components/Header';
+
+import 'bootstrap/dist/css/bootstrap.css';
+
+ReactDOM.render(<Header />, document.getElementById('App'));
